76fe4c4e878ab430788f339b12970b704a0e275a
Ahmed Ezz Eldin <ahmedezeldin.93@gmail.com>
2017-10-17 13:41:39 +0200

    Add unit tests for rank trips

--- trip_ranking/tests/fixtures.py
+++ trip_ranking/tests/fixtures.py
@@ -1,4 +1,8 @@
 import pytest
+import numpy as np
+from datetime import datetime
+
+from trip_ranking.trip_ranking.trips import Trip
 
 
 @pytest.fixture()
@@ -25,3 +29,73 @@ def sliding_window_rank(all_trips_ids):
         five_ranked = rank_trips_id(ranked_five[i::5])
         ranked_final.extend(five_ranked)
     return ranked_final
+
+
+@pytest.fixture
+def dummy_trip_ids():
+    arr = [list(np.random.choice([1, 2, 3, 4, 5], replace=False, size=5)) for _ in range(5)]
+    flat_arr = [item for sublist in arr for item in sublist]
+    return flat_arr
+
+
+@pytest.fixture
+def dummy_trips_1():
+    trips = [
+        Trip(9, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 110, 60),
+        Trip(12, datetime(year=2017, month=10, day=6, hour=12, minute=00), 2, 1, 80, 50),
+        Trip(13, datetime(year=2017, month=10, day=6, hour=12, minute=00), 1, 1, 60, 20),
+        Trip(7, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 60, 30),
+        Trip(14, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 50, 30),
+        Trip(4, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 30, 50),
+        Trip(2, datetime(year=2017, month=10, day=7, hour=12, minute=00), 1, 1, 40, 20),
+        Trip(8, datetime(year=2017, month=10, day=8,    hour=12, minute=00), 2, 1, 90, 50),
+        Trip(6, datetime(year=2017, month=10, day=8, hour=12, minute=00), 4, 1, 120, 100),
+        Trip(18, datetime(year=2017, month=10, day=8, hour=12, minute=00), 2, 1, 120, 50),
+    ]
+    return trips
+
+
+@pytest.fixture
+def dummy_trips_2():
+    trips_2 = [
+        Trip(9, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 110, 60),
+        Trip(12, datetime(year=2017, month=10, day=6, hour=12, minute=00), 2, 1, 80, 50),
+        Trip(13, datetime(year=2017, month=10, day=6, hour=12, minute=00), 1, 1, 60, 20),
+        Trip(7, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 60, 30),
+        Trip(14, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 50, 30),
+        Trip(4, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 30, 50),
+        Trip(2, datetime(year=2017, month=10, day=7, hour=12, minute=00), 1, 1, 40, 20),
+        Trip(8, datetime(year=2017, month=10, day=8,    hour=12, minute=00), 2, 1, 90, 50),
+        Trip(6, datetime(year=2017, month=10, day=8, hour=12, minute=00), 4, 1, 120, 100),
+        Trip(18, datetime(year=2017, month=10, day=8, hour=12, minute=00), 2, 1, 120, 50),
+        Trip(50, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 110, 60),
+        Trip(11, datetime(year=2017, month=10, day=6, hour=12, minute=00), 2, 1, 80, 50),
+        Trip(31, datetime(year=2017, month=10, day=6, hour=12, minute=00), 1, 1, 60, 20),
+        Trip(17, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 60, 30),
+        Trip(22, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 50, 30),
+        Trip(25, datetime(year=2017, month=10, day=8, hour=12, minute=00), 2, 1, 120, 50),
+        Trip(51, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 110, 60),
+        Trip(12, datetime(year=2017, month=10, day=6, hour=12, minute=00), 2, 1, 80, 50),
+        Trip(33, datetime(year=2017, month=10, day=6, hour=12, minute=00), 1, 1, 60, 20),
+        Trip(14, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 60, 30),
+        Trip(25, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 50, 30),
+        Trip(16, datetime(year=2017, month=10, day=8, hour=12, minute=00), 2, 1, 120, 50),
+        Trip(57, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 110, 60),
+        Trip(11, datetime(year=2017, month=10, day=6, hour=12, minute=00), 2, 1, 80, 50),
+        Trip(55, datetime(year=2017, month=10, day=6, hour=12, minute=00), 1, 1, 60, 20),
+        Trip(52, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 60, 30),
+        Trip(88, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 50, 30),
+        Trip(84, datetime(year=2017, month=10, day=8, hour=12, minute=00), 2, 1, 120, 50),
+        Trip(23, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 110, 60),
+        Trip(43, datetime(year=2017, month=10, day=6, hour=12, minute=00), 2, 1, 80, 50),
+        Trip(49, datetime(year=2017, month=10, day=6, hour=12, minute=00), 1, 1, 60, 20),
+        Trip(56, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 60, 30),
+        Trip(88, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 50, 30),
+        Trip(55, datetime(year=2017, month=10, day=8, hour=12, minute=00), 2, 1, 120, 50),
+        Trip(99, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 110, 60),
+        Trip(75, datetime(year=2017, month=10, day=6, hour=12, minute=00), 2, 1, 80, 50),
+        Trip(71, datetime(year=2017, month=10, day=6, hour=12, minute=00), 1, 1, 60, 20),
+        Trip(79, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 60, 30),
+        Trip(69, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 50, 30),
+    ]
+    return trips_2
--- trip_ranking/tests/ranking_trips_test.py
+++ trip_ranking/tests/ranking_trips_test.py
@@ -2,36 +2,9 @@
 Unit Tests for ranking trips module.
 """
 import pytest
-import numpy as np
-from datetime import datetime
 
-from trip_ranking.trip_ranking.trips import Trip
-from trip_ranking.tests.fixtures import sliding_window_rank
+from trip_ranking.tests.fixtures import sliding_window_rank, dummy_trip_ids, dummy_trips_1, dummy_trips_2
 from trip_ranking.trip_ranking import ranking_trips
-
-
-@pytest.fixture
-def dummy_trip_ids():
-    arr = [list(np.random.choice([1, 2, 3, 4, 5], replace=False, size=5)) for _ in range(5)]
-    flat_arr = [item for sublist in arr for item in sublist]
-    return flat_arr
-
-
-@pytest.fixture
-def dummy_trips():
-    trips = [
-        Trip(9, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 110, 60),
-        Trip(12, datetime(year=2017, month=10, day=6, hour=12, minute=00), 2, 1, 80, 50),
-        Trip(13, datetime(year=2017, month=10, day=6, hour=12, minute=00), 1, 1, 60, 20),
-        Trip(7, datetime(year=2017, month=10, day=6, hour=12, minute=00), 3, 1, 60, 30),
-        Trip(14, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 50, 30),
-        Trip(4, datetime(year=2017, month=10, day=7, hour=12, minute=00), 2, 1, 30, 50),
-        Trip(2, datetime(year=2017, month=10, day=7, hour=12, minute=00), 1, 1, 40, 20),
-        Trip(8, datetime(year=2017, month=10, day=8,    hour=12, minute=00), 2, 1, 90, 50),
-        Trip(6, datetime(year=2017, month=10, day=8, hour=12, minute=00), 4, 1, 120, 100),
-        Trip(18, datetime(year=2017, month=10, day=8, hour=12, minute=00), 2, 1, 120, 50),
-    ]
-    return trips
 
 
 def test_ranking_trips_sliding_window_rank_dummy_nums():
@@ -39,6 +12,17 @@ def test_ranking_trips_sliding_window_rank_dummy_nums():
     assert sliding_window_rank(arr) == [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5]
 
 
-def test_ranking_trips():
-    trips = dummy_trips()
-    assert ranking_trips.rank_trips(trips) == [2, 4, 6, 7, 8, 9, 12, 13, 14, 18]
+def test_ranking_trips_less_than_25():
+    trips = dummy_trips_1()
+    assert ranking_trips.rank_trips(trips) == [2, 7, 4, 9, 6, 12, 8, 13, 14, 18]
+
+
+def test_ranking_trips_v2_less_than_25():
+    trips = dummy_trips_1()
+    assert ranking_trips.rank_trips_v2(trips) == [2, 4, 6, 7, 8, 9, 12, 13, 14, 18]
+
+
+def test_ranking_trips_more_than_25():
+    trips = dummy_trips_2()
+    assert ranking_trips.rank_trips(trips) == [2, 7, 11, 11, 12, 4, 9, 14, 16, 17, 6, 12, 22, 25, 25, 8, 13, 31, 33, 
+        55, 14, 18, 50, 51, 57, 23, 49, 43, 55, 52, 56, 84, 88, 88, 99]
