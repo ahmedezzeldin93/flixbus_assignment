27cd709949b7f2929f6d1c81b9d13b0d0b411e43
Ahmed Ezz Eldin <ahmedezeldin.93@gmail.com>
2017-10-17 13:40:26 +0200

    Updated v1 of rank trips and make v2 of rank trips

--- trip_ranking/trip_ranking/ranking_trips.py
+++ trip_ranking/trip_ranking/ranking_trips.py
@@ -18,10 +18,72 @@ def rank_trips(trips:List[Trip]) -> List[int]:
     trips_ranked_window_five = []
     final_ids_ranked = []
     for i in range(0, len(trips) - 4, 5):
-        five_ranked_ids = fixtures.rank_trips(trips[i:(i + 5)])
-        trips_ranked_window_five.extend([trips_dict.get(id) for id in five_ranked_ids])
+        five_ranked_ids = fixtures.rank_trips(trips[i:(i + 5)]) if i+5<len(trips) else fixtures.rank_trips(trips[i:])
+        trips_ranked_window_five.extend([trips_dict.get(id) for id in five_ranked_ids])    
+    
+    for i in range(0, len(trips)-5 if len(trips)>25 else len(trips), 25):
+        for j in range(i, i+5):
+            five_ranked = fixtures.rank_trips(trips_ranked_window_five[j:(j+25):5])
+            final_ids_ranked.extend(five_ranked)
 
-    for i in range(0, len(trips) // 5):
-        five_ranked = fixtures.rank_trips(trips_ranked_window_five[i::5])
-        final_ids_ranked.extend(five_ranked)
     return final_ids_ranked
+
+
+def rank_trips_v2(trips:List[Trip]) -> List[int]:
+    """
+    Rank infinite list of trips using customized merge sort.
+    :param trips: list of trips.
+    :return: list of trip ids.
+    """
+    if len(trips) < 2:
+        return trips
+    elif len(trips) <= 5:
+        return fixtures.rank_trips(trips)         
+    else:
+        ranked_trips = merge_sort_trips(trips)
+        return [trip.trip_id for trip in ranked_trips]
+
+
+def merge_sort_trips(trips_list):
+    """
+    Merge sort algorithm implemented for ranking trips.
+    :param trips: list of trips.
+    :return: list of trip.
+    """
+    if len(trips_list) < 2:
+        return trips_list
+    middle = len(trips_list)//2
+
+    left = merge_sort_trips(trips_list[:middle])
+    right = merge_sort_trips(trips_list[middle:])
+    return merge(left, right)
+
+
+def merge(left_list, right_list):
+    """
+    Merge helper function for merge sort.
+    :param trips: list of trips.
+    :return: list of trip.
+    """
+    if not len(left_list) or not len(right_list):
+        return left_list or right_list
+
+    result = []
+    i, j = 0, 0
+    left_trips_dict = {trip.trip_id: trip for trip in left_list}
+    right_trips_dict = {trip.trip_id: trip for trip in right_list}
+    while (len(result) < len(left_list) + len(right_list)):
+        ranked_two_trips_ids = fixtures.rank_trips([left_list[i],right_list[j]])
+        # if ids[0] belogs to left, ad the trip of id[0] to result and inc the left
+        if ranked_two_trips_ids[0] in left_trips_dict.keys():
+            result.append(left_trips_dict[ranked_two_trips_ids[0]])
+            i+= 1
+        else:
+            result.append(right_trips_dict[ranked_two_trips_ids[0]])
+            j+= 1
+        if i == len(left_list) or j == len(right_list):
+            result.extend(left_list[i:] or right_list[j:])
+            break 
+    return result
+
+
